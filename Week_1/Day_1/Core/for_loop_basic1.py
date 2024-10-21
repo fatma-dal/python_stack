ae0b3f327b46bf51473c8480c52c904d00689b78
fatma-dal <fatmadalhoumi123@gmail.com>
2024-10-21 19:25:43 +0200

    practice

---
+++ Week_1/Day_1/Core/for_loop_basic1.py
@@ -0,0 +1,26 @@
+#
+
+#countdown by four 
+i=2018
+for i in range (2018,i>0,-4):
+    
+    print(i)
+
+
+
+#15
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+
+
+
+
+
---
+++ Week_1/Day_1/Practice/hello_world.py
@@ -0,0 +1,24 @@
+#1
+
+print('hello world!')
+#2
+first_name="fatma"
+print("Hello ",first_name ,"!" )
+#3
+print("*"*20)
+favourite_num=12
+print("my favourite number is : ",favourite_num )
+#3b
+print("*"*10)
+print(f"Hello +{favourite_num} ")
+#4a
+print("&&&"*10)
+fav1="pasta"
+fav2="pizza"
+
+print("My to eat ",fav1," and ",fav2)
+
+
+
+
+
