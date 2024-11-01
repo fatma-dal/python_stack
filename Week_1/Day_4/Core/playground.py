a99cfaf3a300f3eb0cc582515f523ed90c50b4a9
fatma-dal <fatmadalhoumi123@gmail.com>
2024-11-01 10:47:06 +0100

    passing data from the server to the html (table)

--- Week_1/Day_4/Core/playground.py
+++ Week_1/Day_4/Core/playground.py
@@ -7,5 +7,18 @@ def renderboxes(num,backgcolor):
     return render_template('index.html' ,num=num ,backgcolor=backgcolor )
 
 
+# @app.route('/')
+# def get_table():
+#     users = [
+#     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+#     {'first_name' : 'John', 'last_name' : 'Supsupin'},
+#     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]
+#     for key in range (users):
+#         result=users.append(users['key'])
+
+#     return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
