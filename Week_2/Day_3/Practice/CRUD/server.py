fafceef99e5ddad9110c11af3422eb43c511487c
fatma-dal <fatmadalhoumi123@gmail.com>
2024-11-08 15:07:03 +0100

    CRUD

--- Week_2/Day_3/Practice/CRUD/server.py
+++ Week_2/Day_3/Practice/CRUD/server.py
@@ -1,26 +1,57 @@
-from flask import Flask,render_template , redirect,request
+from flask import Flask, render_template, request, redirect
+
+from usermodel import User
+
 app=Flask(__name__)
-from modules.users import User
 
-@app.route('/users')
-def show_all():
-    users_list=User.read_all()
-    return render_template('Read.html',users= users_list)
-
-@app.route('/users/new')
-def show_form():
-    return render_template('create.html')
-
-@app.route('/user/create', methods=['POST'])
-def add_user():
-    User.add_user(request.form)
+@app.route('/')
+def index():
     return redirect('/users')
 
 
+@app.route('/users')
+def users():
+    return render_template("users.html",users=User.get_all())
 
 
+@app.route('/user/new')
+def new():
+    return render_template("new_user.html")
+
+@app.route('/user/create',methods=['POST'])
+def create():
+    print(request.form)
+    User.save(request.form)
+    return redirect('/users')
 
 
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("edit_user.html",user=User.get_one(data))
+
+@app.route('/user/show/<int:id>')
+def show(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("show_user.html",user=User.get_one(data))
+
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data ={
+        'id': id
+    }
+    User.destroy(data)
+    return redirect('/users')
 
 if __name__=="__main__":
     app.run(debug=True)
